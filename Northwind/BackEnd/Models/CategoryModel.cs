07c8b90333363c8d3b07f55a10e954c6f9c64df7
Sebastian Murillo <104529023+Mumogg@users.noreply.github.com>
2024-02-06 19:53:55 -0600

    DI

---
+++ Northwind/BackEnd/Models/CategoryModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BackEnd.Models
+{
+    public class CategoryModel
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } = null!;
+        public string? CategoryDescription { get; set; }
+        public string Description { get; internal set; }
+    }
+}
---
+++ Northwind/BackEnd/Services/Implementations/CategoryService.cs
@@ -0,0 +1,51 @@
+﻿using BackEnd.Models;
+using BackEnd.Services.Interfaces;
+using DAL.Interfaces;
+using Entities.Entities;
+
+namespace BackEnd.Services.Implementations
+{
+    public class CategoryService : ICategoryService
+    {
+        public IUnidadDeTrabajo _unidadDeTrabajo;
+        public bool AddCategory(CategoryModel category)
+        {
+            Category entity = new Category
+            {
+                CategoryId = category.CategoryId,
+                CategoryName = category.CategoryName,
+                Description = category.Description
+            };
+
+            _unidadDeTrabajo._categoryDAL.Add(entity);
+            return _unidadDeTrabajo.Complete();
+        }
+
+        public bool DeleteCategory(CategoryModel category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICategoryService GetById(int id)
+        {
+            var entity = _unidadDeTrabajo._categoryDAL.Get(id);
+            CategoryModel categoryModel = new CategoryModel
+            {
+                CategoryId = entity.CategoryId,
+                CategoryName = entity.CategoryName,
+                Description = entity.Description
+            };
+            return categoryModel;
+        }
+
+        public IEnumerable<CategoryModel> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateCategory(CategoryModel category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
