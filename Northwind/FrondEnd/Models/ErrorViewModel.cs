df81d4983e55be07783c24ff7b921e5db6af9067
Sebastian Murillo <104529023+Mumogg@users.noreply.github.com>
2024-02-07 19:13:23 -0600

    Services and url in appSetings

---
+++ Northwind/FrondEnd/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FrondEnd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
